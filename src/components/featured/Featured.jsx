9f8638fee3bee75f2d0f67e0e238d9f1fba4a5ed
Kp <kp_1@outlook.com>
2022-03-07 21:31:30 +0400

    featured scss update

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,3 +1,4 @@
+import { InfoOutlined, PlayArrow } from "@material-ui/icons"
 import "./featured.scss"
 
 export default function Featured() {
@@ -7,6 +8,28 @@ export default function Featured() {
             src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             alt=""
           />
-        Featured</div>
+        <div className="info">
+            <img src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1"
+                alt= "" />
+            <span className="desc">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Ut praesentium quos recusandae mollitia delectus earum? 
+                Laborum nostrum ipsa voluptates ut culpa quod beatae, 
+                quaerat aut cum veniam reprehenderit dolorum rerum?
+            </span>
+            <div className="buttons">
+                <button className="play">
+                    <PlayArrow />
+                <span>Play</span>
+
+                </button>
+                <button className="more">
+                    <InfoOutlined />
+                    <span>Info</span>
+                </button>
+                 
+            </div>
+            </div>
+        </div>
   )
 }
