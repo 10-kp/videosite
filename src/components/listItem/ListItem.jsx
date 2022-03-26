304f2db24b34d9dbfdb2b6af7124e309e793b56f
Kp <kp_1@outlook.com>
2022-03-26 21:24:01 +0400

    listItem updates

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,7 +1,38 @@
+import { PlayArrow, Add, ThumbUpAltOutlined, ThumbDownAltOutlined } from "@material-ui/icons"
 import "./listItem.scss"
 
 export default function ListItem() {
   return (
-    <div className="listItem"></div>
+    <div className="listItem">
+      <img src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee" 
+      alt="" 
+      />
+    <div className="itemInfo">
+      <div className="icons">
+      <PlayArrow />
+      <Add />
+      <ThumbUpAltOutlined />
+      <ThumbDownAltOutlined />
+
+      </div>
+
+      <div className="itemInfoTop">
+        <span>1 hour 10 min</span>
+        <span className="limit">+18</span>
+        <span>1999</span>
+      </div>
+
+      <div className="desc">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ab voluptatum ullam saepe.
+
+      </div>
+
+        <div className="genre">
+        Action
+        </div>
+
+    </div>
+      
+    </div>
   )
 }
