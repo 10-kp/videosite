16022b1992d684b9a82e2715e4871aff7a8f0ac5
Kp <kp_1@outlook.com>
2022-04-04 21:14:54 +0400

    pages

---
+++ src/pages/watch/Watch.jsx
@@ -0,0 +1,14 @@
+import "./watch.scss"
+
+import React from 'react'
+
+export default function Watch() {
+  return (
+    <div className="watch">
+        <div className="back">
+            
+        </div>
+
+    </div>
+  )
+}
