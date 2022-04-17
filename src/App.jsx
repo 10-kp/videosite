48c7efd006056bfde0bd2da5274d93cb1ee62671
Kp <kp_1@outlook.com>
2022-04-17 20:21:27 +0400

    Final Login updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./app.scss"
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 import Watch from "./pages/watch/Watch";
 // import Login from "./pages/login/Login";
 
 const App = () => {
-  return <Register/>;
+  return <Login/>;
 };
 
 export default App;
