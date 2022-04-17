96e1bb1d2e4996e802699c12303b05b6580315b0
Kp <kp_1@outlook.com>
2022-04-17 19:36:49 +0400

    Tweaks for Register.jsx

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,12 +1,19 @@
+import { useRef } from "react";
+import { useState } from "react";
 import "./register.scss"
 
 export default function Register() {
     const[email,setEmail] = useState("")
+    const[password,setPassword] = useState("")
 
     const emailRef = useRef()
+    const passwordRef = useRef()
 
     const handleStart = () =>{
         setEmail(emailRef.current.value )
+    }
+    const handleFinish = () =>{
+        setPassword(passwordRef.current.value )
     }
   return (
     <div className="register">
@@ -28,13 +35,30 @@ export default function Register() {
             <p>
             Ready to watch? Enter your email to create or restart your membership.
             </p>
-                {!email && (
+                {!email ? (
             <div className="input">
-                <input type="email" placeholder="email address" ref={emailRef}/>
-                <button className="registerButton" onClick={handleStart}>Get Started</button>
+                <input 
+                type="email" 
+                placeholder="email address" 
+                ref={emailRef}/>
+                    <button 
+                    className="registerButton" 
+                    onClick={handleStart}>
+                    Get Started
+                    </button>
             </div>
-                    
-                )}
+                ) : 
+                (
+                <form className="input">
+                <input type="password" 
+                placeholder="password" 
+                ref={passwordRef}/>
+                <button
+                className="registerButton" 
+                onClick={handleFinish}> 
+                Start 
+                </button>
+            </form>)}
 
         </div>
 
