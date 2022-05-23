a581b87ae29858ed76703ee03796aa239a79ce53
Kp <kp_1@outlook.com>
2022-05-23 20:56:04 +0400

    Updates

--- api/index.js
+++ api/index.js
@@ -2,27 +2,36 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+// });
+
+// console.log(process.env);
+// console.log('DB working');
+
 //   .then(() => console.log('DB Connection Successfull'))
 //   .catch((err) => {
 //     console.error(err);
 //   });
-console.log(process.env);
-console.log('DB working');
+
+app.use(express.json());
+
+app.use('/api/auth', authRoute);
 
 // 2nd way for the .env file - Works
-// main().catch((err) => console.log(err));
+main().catch((err) => console.log(err));
 
-// async function main() {
-//   await mongoose.connect('mongodb://localhost:27017/test');
-// }
-// console.log('DB Connection Successful!');
+// CONNECT WITH MONGO_URI!
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/test');
+}
+console.log('DB Connection Successful!');
 
 app.listen(8800, () => {
   console.log('Backend Server is running!');
