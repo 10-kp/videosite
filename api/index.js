593f1ec2a0fb0b8c2cd3014fc2613a3ebbb369ca
Kp <kp_1@outlook.com>
2022-06-05 20:59:49 +0400

    Updates and token

--- api/index.js
+++ api/index.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-
-// dotenv.config();
-require('dotenv').config({ path: './env' });
-
+// const cors = require('cors');
 const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
-
+const dotenv = require('dotenv');
 const authRoute = require('./routes/auth');
-// const userRoute = require('./routes/users');
-// const movieRoute = require('./routes/movies');
-// const listRoute = require('./routes/lists');
+
+dotenv.config();
+// require('dotenv').config({ path: './env' });
 
 mongoose
-  .connect('mongodb://localhost/videoFlix' || MONGO_URL, {
-    useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    //   useCreateIndex: true,
-  })
+  .connect(
+    // 'mongodb://kp:kp11235@cluster1-shard-00-00.qsngh.mongodb.net:27017,cluster1-shard-00-01.qsngh.mongodb.net:27017,cluster1-shard-00-02.qsngh.mongodb.net:27017/?ssl=true&replicaSet=atlas-4qurdv-shard-0&authSource=admin&retryWrites=true&w=majority',
+    // ||
+    process.env.MONGO_URL,
+    {
+      useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      // useCreateIndex: true,
+    }
+  )
   .then(() => console.log('DB Connection Successfull'))
   .catch((err) => {
     console.error(err);
