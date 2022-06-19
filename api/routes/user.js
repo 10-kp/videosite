e2f9d5b49203920e29826dd16c25aaa6cf818988
Kp <kp_1@outlook.com>
2022-06-19 20:57:55 +0400

    Verify Token

---
+++ api/routes/user.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const User = require('./models/User');
+const CryptoJS = require('crypto-js');
+
+// UPDATE
+
+// DELETE
+
+// GET
+
+// GET ALL
+
+// GET USER STAT
