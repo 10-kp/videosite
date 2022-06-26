e1fc5ea1d83b3d50b100779bd06e44cb258013a5
Kp (Kunal) <kp_1@outlook.com>
2022-06-26 21:01:34 +0400

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
-const User = require('./models/User');
-const CryptoJS = require('crypto-js');
-const verify = require('../verifyToken');
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const verify = require("../verifyToken");
 
 // UPDATE
 router.put('/:id', verify, async (req, res) => {
